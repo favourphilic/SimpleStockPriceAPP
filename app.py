51f0f1abf187dde5e5a5dc06c50d9ec1ca14d2ab
Victor A Jokanola <63900671+favourphilic@users.noreply.github.com>
2023-10-19 14:26:50 +0100

    new commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import streamlit as st
+
+st.title("Simple Stock Price App")
+st.subheader("Shown are the stock closing price and volume of Google!")
+
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
